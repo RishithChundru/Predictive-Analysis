0fe5db9cc924ac27601c86dfae4bf87a4879190e
Rishith <rishithrishith1@gmail.com>
2024-10-18 10:52:12 +0530

    new files

---
+++ All Regression model.R
@@ -0,0 +1,42 @@
+# predict petal length using sepal width
+
+# Single linear regression
+x<-iris
+x
+a<-data.frame(x)
+plot(a$Sepal.Width,a$Petal.Length)
+rel<-lm(Petal.Length~Sepal.Width,data=a)
+abline(rel,col="red",lwd=2)
+summary(rel)
+predict(rel,data.frame(Sepal.Width=3.2))
+
+
+# Multiple linear regression
+plot(a$Sepal.Width,a$Petal.Length)
+rel1<-lm(Petal.Length~Sepal.Length+Sepal.Width+Petal.Width,data=a)
+abline(lm(Petal.Length~Sepal.Width,data=a),col="blue",lwd=2)
+summary(rel)
+predict(rel1,data.frame(Sepal.Length=4.5,Sepal.Width=3.2,Petal.Width=5.6))
+
+
+#polynomial regression
+library(ggplot2)
+a$Slength=a$Sepal.Length^2
+a$swidth=a$Sepal.Width^3
+a$pwidth=a$Petal.Width^4
+View(a)
+plot(a$Sepal.Width,a$Petal.Length)
+rel<-lm(Petal.Length~Slength+swidth+pwidth,data=a)
+ggplot()+geom_point(aes(x=a$Sepal.Width,y=a$Petal.Length),colour="red")+geom_line(aes(x=a$Sepal.Width,y=predict(rel,newdata = a)),colour="blue")+ggtitle("Iris Data")+xlab("Sepal Width")+ylab("Petal Length")
+predict(rel,data.frame(Slength=3.4^2,swidth=3.4^3,pwidth=3.4^4))
+
+
+########
+model2 <- lm(Petal.Length ~ poly(Sepal.Width, 2), data = a)
+summary(model2)
+
+summary(model)$r.squared
+summary(model2)$r.squared
+
+plot(a$Sepal.Width, a$Petal.Length)
+lines(sort(a$Sepal.Width), predict(model2, data.frame(Sepal.Width = sort(a$Sepal.Width))), col = "blue",lwd=2)
